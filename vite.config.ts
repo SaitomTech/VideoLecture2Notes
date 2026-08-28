4260958eeaf0d4811d9e9885c35b41179cea0432
SaitomTech <57802435+SaitomTech@users.noreply.github.com>
2026-08-28 15:08:53 +0900

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,11 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 1420,
+    strictPort: true,
+    watch: {
+      ignored: ['**/src-tauri/**'],
+    },
+  },
 })
